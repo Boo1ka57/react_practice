bd14d2b64d7b0c62068376ad049b4b8d97b20235
Boo1ka57 <116977824+Boo1ka57@users.noreply.github.com>
2022-11-29 13:53:22 +0300

    Add files via upload

---
+++ practiceProps.tsx
@@ -0,0 +1,13 @@
+// const App = ({title, score}: { title: string, score:number}) => (
+// //     <h1>{title} = {score}</h1>
+// // )
+//
+// type Props = {
+//     onClick: () => void,
+//     onChange: (target: string) => boolean,
+//     handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void
+// }
+//
+// const App = ({title, score}: Props) => (
+//     <h1>{title} = {score}</h1>
+// )
